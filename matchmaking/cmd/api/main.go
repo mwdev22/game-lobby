9bb1de85a5ffc4d66c6632def210e19273ca60f8
mwdev22 <maciekwojdyna1@gmail.com>
2025-11-12 23:46:43 +0100

    init structure for matchmaking svc

---
+++ matchmaking/cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	config "github.com/mwdev22/gocfg"
+	"github.com/mwdev22/grpclib/grpcserver"
+	"github.com/mwdev22/matchmaking/api"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	cfg := config.New(
+		config.WithDatabaseConfig(
+			&config.DatabaseConfig{},
+		),
+	)
+
+	server := grpcserver.New(
+		cfg.Addr,
+		grpcserver.WithCreds(insecure.NewCredentials()),
+	)
+
+	app := api.New(cfg, server)
+
+	ctx := context.Background()
+
+	if err := app.Start(ctx); err != nil {
+		log.Fatalf("error while running app: %v", err)
+	}
+}
---
+++ matchmaking/domain.go
@@ -0,0 +1,3 @@
+package matchmaking
+
+// here is the place where u define all domain objects and interfaces used by application
